4bec3c33d96abd0c9fb9a837c49267b951efb235
Dhiraj <dhirajwasu120@gmail.com>
2024-01-19 16:33:21 +0530

    new file:   quicksort.cpp

---
+++ quicksort.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int arr[]={10, 80, 30, 90, 40};
+    int pivot= arr[4];
+    for int i = 0; i < ; i++)
+    {
+        /* code */
+    }
+    
+    return 0;
+}
